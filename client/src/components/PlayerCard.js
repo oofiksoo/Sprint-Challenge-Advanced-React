1fe5ad58327e5aa38f6bfa785c25c45cfd7422ac
Joseph Graham <Joseph@Chasingthefiks.net>
2019-12-06 10:59:53 -0700

    adjusted padding

--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -5,10 +5,10 @@ export default props => {
     <div className="playerCardSpace">
       {props.players.map(player => (
         <div className="playerCard">
-          <h2>{player.name}</h2>
+          <h1>{player.name}</h1>
           <h3>{player.country}</h3>
           <div className="searchCount">
-            <h1>Search Count:{player.searches}</h1>
+            <h2>Search Count:{player.searches}</h2>
           </div>
         </div>
       ))}
