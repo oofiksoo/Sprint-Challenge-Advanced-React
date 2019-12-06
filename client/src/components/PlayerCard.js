ed60a93877fc5247a5341c92410a9a24d7a24b62
Joseph Graham <Joseph@Chasingthefiks.net>
2019-12-06 10:53:15 -0700

    added minor style to make it presentable.

--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -7,7 +7,9 @@ export default props => {
         <div className="playerCard">
           <h2>{player.name}</h2>
           <h3>{player.country}</h3>
-          <h1>Search Count:{player.searches}</h1>
+          <div className="searchCount">
+            <h1>Search Count:{player.searches}</h1>
+          </div>
         </div>
       ))}
     </div>
